5d07efc2dcdcbbcaa73bb622058d6d8696d8c64d
sofiasaless <sofiasaleswk@gmail.com>
2025-11-24 15:13:04 -0300

    feat: criação do auth guard para autenticação

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmpresaModule } from './empresa/empresa.module';
 import { ProdutoModule } from './produto/produto.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ProdutoModule,
-    EmpresaModule
+    EmpresaModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
